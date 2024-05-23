8552d840e28534e67ba874f8637931ade1ad1c28
rampagekiller0725 <113688176+rampagekiller0725@users.noreply.github.com>
2024-05-24 00:18:25 +0900

    footer active

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -51,7 +51,7 @@ const Footer = ({username}) => {
             {
                 data.map((d, i) => {
                     return (
-                        <Link to={d.link} key={i} onClick={() => handleActiveClicked(d.active)} className={`flex flex-col p-2 w-full ${active === d.active ? "bg-[#1C1F24] rounded-md text-[#FEFEFE]" : "text-[#7C7C7C]"}`}>
+                        <Link to={d.link} key={i} onClick={() => handleActiveClicked(d.active)} className={`flex flex-col p-2 w-full ${active.indexOf(d.active) != -1 ? "bg-[#1C1F24] rounded-md text-[#FEFEFE]" : "text-[#7C7C7C]"}`}>
                             <div  className='flex flex-col gap-1 items-center'>
                                 <img width={30} height={30} src={d.image} alt='' />
                                 <p className='text-[14px]'>{d.title}</p>
