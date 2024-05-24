93be1789bb58aae97d56069fc11d8d0c0f5f6af4
rampagekiller0725 <113688176+rampagekiller0725@users.noreply.github.com>
2024-05-25 04:31:41 +0900

    back

--- src/components/Navigation/nav.jsx
+++ src/components/Navigation/nav.jsx
@@ -1,6 +1,7 @@
 // Nav.tsx
 import React from 'react';
 import { TbChevronLeft, TbX } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
 // import Image from 'next/image';
 // import { HiOutlineDotsVertical } from "react-icons/hi";
 
@@ -10,9 +11,9 @@ const Nav = ({ handleClick, showNav, username }) => {
     return (
         <div className={`py-2 flex pb-2 items-center gap-[25%] w-full text-white px-[4%] bg-black`}>
             {/* <TbX className='text-3xl font-bold' onClick={handleClick} />  */}
-            <a href={"/"}>
+            <Link to={"/exchange/"+username}>
                 <TbChevronLeft className='text-3xl'/>
-            </a>
+            </Link>
             <div className='flex flex-row items-center gap-3'>
                 <img width={25} height={25} src={"/logo.svg"} alt='' />
                 <p className='text-[18px] font-bold text-[#FEFEFE]'>{username}</p>
